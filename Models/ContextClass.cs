15dc16196a749990465bfc754be7eff41399699a
roshni1310 <47430089+roshni1310@users.noreply.github.com>
2019-06-28 14:14:54 -0400

    Initial commit

---
+++ Models/ContextClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace toDo.Models
+{
+    public class ContextClass:DbContext
+    {
+        public ContextClass(DbContextOptions<ContextClass> options):base(options)
+        {
+
+        }
+        public DbSet<ItemClass> TodoItems { get; set; }
+    }
+}
